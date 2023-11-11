5ce40af365f63533cbf83d56ae9a6ed7efe828a2
ubyhzargam <ubyhzargam@gmail.com>
2023-11-11 23:32:48 +0530

    Update main.cpp

--- queue_using_linkedlist/main.cpp
+++ queue_using_linkedlist/main.cpp
@@ -1,14 +1,70 @@
-//
-//  main.cpp
-//  queue_using_linkedlist
-//
-//  Created by Uby H on 09/11/23.
-//
+#include<iostream>
+using namespace std;
 
-#include <iostream>
+struct Node
+{
+    int data;
+    Node *next;
+}*first=NULL,*last=NULL;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void enqueue(int x)
+{
+    struct Node *n=(Node*)malloc(sizeof(Node));
+    if(n==NULL)
+        cout<<"Queue is Full "<<endl;
+    else if(first==NULL)
+    {
+        n->data=x;
+        n->next=NULL;
+        first=n;
+        last=first;
+    }
+    else
+    {
+        n->data=x;
+        n->next=NULL;
+        last->next=n;
+        last=n;
+    }
+}
+
+int dequeue()
+{
+    int x=-1;
+    if(first==NULL)
+    {
+        cout<<"The queue is empty, there is nothing to delete "<<endl;
+    }
+    else
+    {
+        struct Node *p=new Node;
+        p=first;
+        first=first->next;
+        x=p->data;
+        free(p);
+    }
+    return x;
+}
+
+void Display()
+{
+    struct Node *p=new Node;
+    p=first;
+    cout<<"The elements in the queue are given by : "<<endl;
+    if(first==NULL)
+        cout<<"There are no elements in the queue, the queue is empty "<<endl;
+    else
+    {
+        while(p!=NULL)
+        {
+            cout<<p->data<<" ";
+            p=p->next;
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
     return 0;
 }
